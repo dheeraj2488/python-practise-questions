def71d0eed28b6e51c082db97b047a1e65399e6a
Dheeraj thakur <115392626+dheeraj2488@users.noreply.github.com>
2022-12-21 10:14:38 +0530

    Add files via upload

---
+++ assignment 21 python/que14.py
@@ -0,0 +1,6 @@
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+d={}
+for i in employees:
+    d[i]=defaults
+print(d)    
---
+++ assignment 21 python/que15.py
@@ -0,0 +1,9 @@
+# Keys to extract
+# keys = ["name", "salary"]
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+    
+
